77f01e72794fb2b53393f381759c466bc54e1576
Marc Bastian Heinrichs <typo3@mbh-software.de>
2015-08-05 18:59:34 +0200

    [TASK] Remove xclass include

--- sv1/class.tx_svconnectorcsv_sv1.php
+++ sv1/class.tx_svconnectorcsv_sv1.php
@@ -238,8 +238,5 @@ class tx_svconnectorcsv_sv1 extends tx_svconnector_base {
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/svconnector_csv/sv1/class.tx_svconnectorcsv_sv1.php']) {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/svconnector_csv/sv1/class.tx_svconnectorcsv_sv1.php']);
-}
 
 ?>
